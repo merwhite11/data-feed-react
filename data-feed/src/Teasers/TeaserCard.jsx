f7fc4527c3842bf89d0cb9755c610e3aba7d7548
merwhite11 <meredith.white91@gmail.com>
2024-05-24 11:49:51 -0700

    Mobile formatted and Tablet somewhat formatted

--- data-feed/src/Teasers/TeaserCard.jsx
+++ data-feed/src/Teasers/TeaserCard.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import TeaserList from './TeaserList';
+import './Teasers.scss'
 
 function TeaserCard({image, title, summary}) {
   console.log('title', title)
   return (
-  <div>
-    <div>{title}</div>
-    <div>{summary}</div>
+  <div className="teaser text-center justify-content-center">
+    <section>
+      <img className="teaser__image" src={image}>
+      </img>
+    </section>
+    <section className="teaser__text d-flex flex-column justify-content-center">
+    <h4 className="teaser__title text-center">{title}</h4>
+    <p>{summary}</p>
+    </section>
   </div>
   )
 }
 
-export default TeaserCard
+export default TeaserCard;
+
+/*
+tablet:
+
+*/
