61db69f246002238e276842be002f9a3d56fbaff
merwhite11 <meredith.white91@gmail.com>
2024-05-29 10:58:46 -0700

    Tablet Styling complete

--- data-feed/src/Teasers/TeaserCard.jsx
+++ data-feed/src/Teasers/TeaserCard.jsx
@@ -7,13 +7,13 @@ function TeaserCard({image, title, summary}) {
   console.log('title', title)
   return (
   <div className="teaser text-center justify-content-center">
-    <section>
+    <section className="teaser__image-container">
       <img className="teaser__image" src={image}>
       </img>
     </section>
-    <section className="teaser__text d-flex flex-column justify-content-center">
-    <h4 className="teaser__title text-center">{title}</h4>
-    <p>{summary}</p>
+    <section className="teaser__text d-flex flex-column justify-content-start">
+    <h4 className="teaser__title justify-content-center">{title}</h4>
+    <p className="teaser__summary">{summary}</p>
     </section>
   </div>
   )
